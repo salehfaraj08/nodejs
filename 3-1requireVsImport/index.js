df8e4c5fddefd13cbf55ac6c974fa6b5ad87bee5
salehfaraj08 <saleh.faraj08@gmail.com>
2021-11-08 00:56:44 +0200

    3-1import

---
+++ 3-1requireVsImport/index.js
@@ -0,0 +1,18 @@
+/* differences between import and require
+import:
+  Lexical will be sorted to the top of the current file.
+  Only can be called in the beginning.
+  Able to load partially(saving a lot of memories).
+    require:
+Non - lexical; stay wherever it called.
+  Can be called anytime.
+  Not able to load partially. */
+import name, { func1, func2, func3 } from './module.js'
+import fs from 'fs';
+
+
+
+console.log(name);
+console.log(func1());
+console.log(func2());
+console.log(func3());
---
+++ 3-1requireVsImport/module.js
@@ -0,0 +1,12 @@
+const name = "saleh";
+export default name;
+
+export function func1() {
+    return 'func1';
+}
+export function func2() {
+    return 'func2';
+}
+export function func3() {
+    return 'func3';
+}
